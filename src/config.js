9eaf2397ca6ad6dc99960543fafa615ba8e2c74a
weixianfa <372301467@qq.com>
2020-01-04 13:59:48 +0800

    init

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// 这里存小程序的 appid 和 appsecret
+// 登录公众号后打开这个网页就可以看到了：https://mp.weixin.qq.com/wxamp/devprofile/get_profile?token=1348860549&lang=zh_CN
+// appid 和 appsecret 属于敏感数据，请严加保管
+
+module.exports = {
+  appid: '123',
+  appsecret: '456'
+}
